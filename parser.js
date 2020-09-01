0b56e6cb2355ac96e9862a936f7d07458400722a
mark <markpackham1@gmail.com>
2020-09-01 13:04:00 +0100

    make the app less static

--- parser.js
+++ parser.js
@@ -1,5 +1,7 @@
 // Amazon Price Tracker learned from https://www.youtube.com/watch?v=H5ObmDUjKV4
-// to run just do "node parser.js"
+// example url for test "https://www.amazon.co.uk/WD-Elements-Portable-External-Drive/dp/B06VVS7S94"
+// we want to know if the price has dropped below £30 so we supply it 30
+// to run just do the following supplying a url and a min price "node parser.js https://www.amazon.co.uk/WD-Elements-Portable-External-Drive/dp/B06VVS7S94" 30"
 
 require("dotenv").config();
 const sgMail = require("@sendgrid/mail");
@@ -10,19 +12,15 @@ const url = args[0];
 const minPrice = args[1];
 
 checkPrice();
-
 async function checkPrice() {
   const priceString = await nightmare
-    .goto(
-      "https://www.amazon.co.uk/WD-Elements-Portable-External-Drive/dp/B06VVS7S94"
-    )
+    .goto(url)
     .wait("#priceblock_ourprice")
     .evaluate(() => document.getElementById("priceblock_ourprice").innerText)
     .end();
 
   const priceNumber = parseFloat(priceString.replace("£", ""));
-  // we want to know if the price has dropped below £30
-  if (priceNumber < 30) {
+  if (priceNumber < minPrice) {
     console.log("It is cheap at less than £30");
   } else {
     console.log("It is still expensive");
